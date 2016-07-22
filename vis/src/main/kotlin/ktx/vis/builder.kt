c6aa429537f1a1623fbb3d84addc1b5ea7e56aa4
Paweł Pastuszak <kotcrab@gmail.com>
2016-07-22 11:39:35 +0200

    Inlined builder functions, #5

--- vis/src/main/kotlin/ktx/vis/builder.kt
+++ vis/src/main/kotlin/ktx/vis/builder.kt
@@ -13,37 +13,37 @@ import com.kotcrab.vis.ui.widget.VisTable
 /** @author Kotcrab */
 
 /** Begins creation of UI using type-safe builder, uses [VisTable] as base widget container. */
-fun table(init: KVisTable.() -> Unit): VisTable = actor(KVisTable(), init)
+inline fun table(init: KVisTable.() -> Unit): VisTable = actor(KVisTable(), init)
 
 /** Begins creation of UI using type-safe builder, uses [HorizontalGroup] as base widget container. */
-fun horizontalGroup(init: KHorizontalGroup.() -> Unit): HorizontalGroup = actor(KHorizontalGroup(), init)
+inline fun horizontalGroup(init: KHorizontalGroup.() -> Unit): HorizontalGroup = actor(KHorizontalGroup(), init)
 
 /** Begins creation of UI using type-safe builder, uses [HorizontalFlowGroup] as base widget container. */
-fun horizontalFlowGroup(spacing: Float = 0f, init: KHorizontalFlowGroup.() -> Unit): HorizontalFlowGroup
+inline fun horizontalFlowGroup(spacing: Float = 0f, init: KHorizontalFlowGroup.() -> Unit): HorizontalFlowGroup
     = actor(KHorizontalFlowGroup(spacing), init)
 
 /** Begins creation of UI using type-safe builder, uses [VerticalGroup] as base widget container. */
-fun verticalGroup(init: KVerticalGroup.() -> Unit): VerticalGroup = actor(KVerticalGroup(), init)
+inline fun verticalGroup(init: KVerticalGroup.() -> Unit): VerticalGroup = actor(KVerticalGroup(), init)
 
 /** Begins creation of UI using type-safe builder, uses [VerticalFlowGroup] as base widget container. */
-fun verticalFlowGroup(spacing: Float = 0f, init: KVerticalFlowGroup.() -> Unit): VerticalFlowGroup
+inline fun verticalFlowGroup(spacing: Float = 0f, init: KVerticalFlowGroup.() -> Unit): VerticalFlowGroup
     = actor(KVerticalFlowGroup(spacing), init)
 
 /** Begins creation of UI using type-safe builder, uses [GridGroup] as base widget container. */
-fun gridGroup(itemSize: Float = 256f, spacing: Float = 8f, init: KGridGroup.() -> Unit): GridGroup
+inline fun gridGroup(itemSize: Float = 256f, spacing: Float = 8f, init: KGridGroup.() -> Unit): GridGroup
     = actor(KGridGroup(itemSize, spacing), init)
 
 /** Begins creation of UI using type-safe builder, uses [FloatingGroup] as base widget container. */
-fun floatingGroup(init: KFloatingGroup.() -> Unit): FloatingGroup = actor(KFloatingGroup(), init)
+inline fun floatingGroup(init: KFloatingGroup.() -> Unit): FloatingGroup = actor(KFloatingGroup(), init)
 
 /** Begins creation of UI using type-safe builder, uses [FloatingGroup] as base widget container. */
-fun floatingGroup(prefWidth: Float, prefHeight: Float, init: KFloatingGroup.() -> Unit): FloatingGroup
+inline fun floatingGroup(prefWidth: Float, prefHeight: Float, init: KFloatingGroup.() -> Unit): FloatingGroup
     = actor(KFloatingGroup(prefWidth, prefHeight), init)
 
 /** Begins creation of UI using type-safe builder, uses [Stack] as base widget container. */
-fun stack(init: KStack.() -> Unit): Stack = actor(KStack(), init)
+inline fun stack(init: KStack.() -> Unit): Stack = actor(KStack(), init)
 
-internal fun <T : Actor> actor(actor: T, init: T.() -> Unit): T {
+inline fun <T : Actor> actor(actor: T, init: T.() -> Unit): T {
   actor.init()
   return actor
 }
