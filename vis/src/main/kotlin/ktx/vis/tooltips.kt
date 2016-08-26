b14fa00ffebf73216a0422a1d90f0bfec1afb087
Paweł Pastuszak <kotcrab@gmail.com>
2016-08-26 11:50:15 +0200

    Added extension methods for tooltips, #5

---
+++ vis/src/main/kotlin/ktx/vis/tooltips.kt
@@ -0,0 +1,28 @@
+package ktx.vis
+
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.utils.Align
+import com.kotcrab.vis.ui.widget.Tooltip
+import com.kotcrab.vis.ui.widget.VisLabel
+
+/** @author Kotcrab */
+
+/** Creates and adds text [Tooltip] to [Actor] */
+fun Actor.addTooltip(content: Actor, styleName: String = DEFAULT_STYLE, init: Tooltip.() -> Unit = {}): Tooltip {
+  val tooltip = Tooltip(styleName)
+  tooltip.content = content
+  tooltip.setTarget(this)
+  tooltip.init()
+  return tooltip
+}
+
+/** Creates and adds [Tooltip] to [Actor] */
+fun Actor.addTextTooltip(text: String, textAlign: Int = Align.center, styleName: String = DEFAULT_STYLE, init: Tooltip.() -> Unit = {}): Tooltip {
+  val label = VisLabel(text)
+  label.setAlignment(textAlign)
+  val tooltip = Tooltip(styleName)
+  tooltip.content = label
+  tooltip.setTarget(this)
+  tooltip.init()
+  return tooltip
+}
