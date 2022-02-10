312d003bfd6b921aba6719387ab1b9498f14ddf2
RedGrapefruit <karpovanton729@gmail.com>
2022-02-10 17:42:02 +0300

    Typo fixes; entity listener utils part 1

---
+++ ashley/src/main/kotlin/ktx/ashley/entity_listeners.kt
@@ -0,0 +1,26 @@
+package ktx.ashley
+
+import com.badlogic.ashley.core.Entity
+import com.badlogic.ashley.core.EntityListener
+
+/**
+ * An [EntityListener] only listening to the [entityAdded] event.
+ *
+ * A [Unit] implementation is provided for the [entityRemoved] event, as it is ignored.
+ *
+ * If you need both events from the [EntityListener], implement the regular [EntityListener] from Ashley.
+ */
+interface EntityAdditionListener : EntityListener {
+  override fun entityRemoved(entity: Entity) = Unit
+}
+
+/**
+ * An [EntityListener] only listening to the [entityRemoved] event.
+ *
+ * A [Unit] implementation is provided for the [entityAdded] event, as it is ignored.
+ *
+ * If you need both events from the [EntityListener], implement the regular [EntityListener] from Ashley.
+ */
+interface EntityRemovalListener : EntityListener {
+  override fun entityAdded(entity: Entity) = Unit
+}
