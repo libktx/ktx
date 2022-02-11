325e254cc8614095660e98edb7291bf9fcefb4fc
RedGrapefruit <karpovanton729@gmail.com>
2022-02-11 15:42:55 +0300

    Fix tests

--- ashley/src/test/kotlin/ktx/ashley/listenerTest.kt
+++ ashley/src/test/kotlin/ktx/ashley/listenerTest.kt
@@ -6,12 +6,12 @@ import com.badlogic.ashley.core.Entity
 class `should implement EntityAdditionListener with only one method implemented` : EntityAdditionListener {
   // Guarantees EntityAdditionListener's entityRemoved method is always optional to implement
 
-  override fun entityAdded(entity: Entity?) = Unit
+  override fun entityAdded(entity: Entity) = Unit
 }
 
 @Suppress("unused")
 class `should implement EntityRemovalListener with only one method implemented` : EntityRemovalListener {
   // Guarantees EntityRemovalListener's entityAdded method is always optional to implement
 
-  override fun entityRemoved(entity: Entity?) = Unit
+  override fun entityRemoved(entity: Entity) = Unit
 }
